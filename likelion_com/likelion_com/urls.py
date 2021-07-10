4605249f0d2564f979972395e4c3a91a196ef7e6
SunnyBaik <qortmdfuf007@hotmail.com>
2021-07-10 20:15:02 +0900

    created Post model

--- likelion_com/likelion_com/urls.py
+++ likelion_com/likelion_com/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import privateCommunity.views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', privateCommunity.views.home, name='home'),
+    path('community/', include('privateCommunity.urls')), 
 ]
