ebc8de50d7c079e708e34bc54846acc29b66a3d6
SunnyBaik <qortmdfuf007@hotmail.com>
2021-07-10 20:59:02 +0900

    profile model update

--- likelion_com/accounts/models.py
+++ likelion_com/accounts/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.db.models.fields import TextField
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
-class Profile(models.Model):   # 추가
+class Profile(models.Model):
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)    
     college = models.CharField(max_length=20, blank=True, null=True)    
@@ -14,4 +16,15 @@ class Profile(models.Model):   # 추가
     email = models.CharField(blank=True, null=True)
     tmi = models.TextField()
     github_id = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return f'id={self.id}, user_id={self.user.id}, college={self.college}, batch={self.batch}'
+
+    @receiver(post_save, sender=User)  
+    def create_user_profile(sender, instance, created, **kwargs):        
+        if created:          
+            Profile.objects.create(user=instance)  
     
+    @receiver(post_save, sender=User)  
+    def save_user_profile(sender, instance, **kwargs):        
+        instance.profile.save()
