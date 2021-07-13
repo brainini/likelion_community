32c77cf44f69f4d676cee3759f612f3fafe736ca
SunnyBaik <45480319+SunnyBaik@users.noreply.github.com>
2021-07-13 20:41:28 +0900

    Delete likelion_com directory

--- likelion_com/privateCommunity/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrivatecommunityConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'privateCommunity'
--- likelion_com/privateCommunity/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from privateCommunity import views
-
-urlpatterns = [
-    path('', views.home, name='home'), # 'localhost:8000/community/'
-]
