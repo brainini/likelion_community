cc93ccbbd7d65472003d4fb6327347f6fa3356ee
brainini <hantaegu96@naver.com>
2021-07-23 02:45:00 +0900

    [file] css, css.map file

--- likelion_com/privateCommunity/views.py
+++ likelion_com/privateCommunity/views.py
@@ -21,12 +21,12 @@ def home(request):
         emotions = post.emotion_set
         return render(request, 'privateCommunity/show.html', {'post':post, 'comments':comments, 'emotions':emotions})
     else:
-        notice = Post.objects.filter(category='공지게시판').order_by('-created_at')[0:4]
-        announce = Post.objects.filter(category='공고게시판').order_by('-created_at')[0:4]
-        qna = Post.objects.filter(category='질의응답게시판').order_by('-created_at')[0:4]
-        apply = Post.objects.filter(category='모집게시판').order_by('-created_at')[0:4]
-        free = Post.objects.filter(category='자유게시판').order_by('-created_at')[0:4] 
-        info = Post.objects.filter(category='정보게시판').order_by('-created_at')[0:4]
+        notice = Post.objects.filter(category='공지게시판').order_by('-created_at')[0:5]
+        announce = Post.objects.filter(category='공고게시판').order_by('-created_at')[0:5]
+        qna = Post.objects.filter(category='질의응답게시판').order_by('-created_at')[0:5]
+        apply = Post.objects.filter(category='모집게시판').order_by('-created_at')[0:5]
+        free = Post.objects.filter(category='자유게시판').order_by('-created_at')[0:5] 
+        info = Post.objects.filter(category='정보게시판').order_by('-created_at')[0:5]
 
         return render(request, 'privateCommunity/home.html', {'notice':notice, 'announce':announce, 'qna':qna, 'apply':apply, 'free':free, 'info':info})
 
